da4ee308397991af1f5011e6887d7ec518822fbe
Daniel Reigada <daniel.reigada@gmail.com>
2018-04-11 14:37:39 +0100

    Add WithGenericFunctions class

---
+++ src/main/java/ist/meic/pa/GenericFunctions/WithGenericFunctions.java
@@ -0,0 +1,27 @@
+package ist.meic.pa.GenericFunctions;
+
+import javassist.ClassPool;
+import javassist.Loader;
+import javassist.Translator;
+
+import java.util.Arrays;
+
+public class WithGenericFunctions {
+  public static void main(String[] args) throws Throwable {
+    if (args.length < 1) {
+      throw new RuntimeException("Missing class name argument");
+    }
+
+    Translator t = new GenericFunctionsTranslator();
+    ClassPool pool = ClassPool.getDefault();
+    Loader cl = new Loader();
+    cl.addTranslator(pool, t);
+
+
+    String className = args[0];
+    String[] newArgs = Arrays.copyOfRange(args, 1, args.length);
+
+    cl.run(className, newArgs);
+  }
+
+}
