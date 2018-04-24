a79a58abbacce040e159f3fbec6e1d40fff807ce
Daniel Reigada <daniel.reigada@gmail.com>
2018-04-24 13:11:57 +0100

    Change benchmarks time to ms

--- src/test/java/ist/meic/pa/GenericFunctionsExtended/benchmarks/CacheVsNoCacheTimeBenchmark.java
+++ src/test/java/ist/meic/pa/GenericFunctionsExtended/benchmarks/CacheVsNoCacheTimeBenchmark.java
@@ -4,7 +4,6 @@ package ist.meic.pa.GenericFunctionsExtended.benchmarks;
 import ist.meic.pa.GenericFunctionsExtended.benchmarks.domain.C1;
 import ist.meic.pa.GenericFunctionsExtended.benchmarks.domain.TiCache;
 import ist.meic.pa.GenericFunctionsExtended.benchmarks.domain.TiNoCache;
-import ist.meic.pa.GenericFunctionsExtended.benchmarks.domain.TiNoGeneric;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -14,7 +13,6 @@ public class CacheVsNoCacheTimeBenchmark {
 
   public static List<Long> cacheTimes = new LinkedList<>();
   public static List<Long> noCacheTimes = new LinkedList<>();
-  public static List<Long> noGenericTimes = new LinkedList<>();
 
   public static void main(String[] args) {
     Object o = new C1();
@@ -25,36 +23,26 @@ public class CacheVsNoCacheTimeBenchmark {
 
     OptionalDouble cacheAverage = cacheTimes.stream().mapToLong(a -> a).average();
     OptionalDouble noCacheAverage = noCacheTimes.stream().mapToLong(a -> a).average();
-    OptionalDouble noGenericAverage = noGenericTimes.stream().mapToLong(a -> a).average();
 
-    System.out.println("Cache average " + cacheAverage.getAsDouble() * 1000 + "ns");
-    System.out.println("NoCache average " + noCacheAverage.getAsDouble() * 1000 + "ns");
-    System.out.println("NoGeneric average " + noGenericAverage.getAsDouble() * 1000 + "ns");
+    System.out.println("Cache average " + cacheAverage.getAsDouble() + "ms");
+    System.out.println("NoCache average " + noCacheAverage.getAsDouble() + "ms");
   }
 
   public static void runTest(Object o, int runs) {
-    long time = System.nanoTime();
+    long time = System.currentTimeMillis();
     for (int i = 0; i < runs; i++) {
       TiCache.me(o);
     }
-    time = System.nanoTime() - time;
+    time = System.currentTimeMillis() - time;
     System.out.println("Cache: " + time + "ms");
     cacheTimes.add(time);
 
-    time = System.nanoTime();
+    time = System.currentTimeMillis();
     for (int i = 0; i < runs; i++) {
       TiNoCache.me(o);
     }
-    time = System.nanoTime() - time;
+    time = System.currentTimeMillis() - time;
     System.out.println("NoCache: " + time + "ms");
     noCacheTimes.add(time);
-
-    time = System.nanoTime();
-    for (int i = 0; i < runs; i++) {
-      TiNoGeneric.me((C1) o);
-    }
-    time = System.nanoTime() - time;
-    System.out.println("NoGeneric: " + time + "ms");
-    noGenericTimes.add(time);
   }
 }
--- src/test/java/ist/meic/pa/GenericFunctionsExtended/benchmarks/CacheVsNoGenericTimeBenchmark.java
+++ src/test/java/ist/meic/pa/GenericFunctionsExtended/benchmarks/CacheVsNoGenericTimeBenchmark.java
@@ -17,30 +17,30 @@ public class CacheVsNoGenericTimeBenchmark {
     Object o = new C1();
 
     for (int i = 0; i < 10; i++) {
-      runTest(o, 1000000);
+      runTest(o, 100000);
     }
 
     OptionalDouble cacheAverage = cacheTimes.stream().mapToLong(a -> a).average();
     OptionalDouble noGenericAverage = noGenericTimes.stream().mapToLong(a -> a).average();
 
-    System.out.println("Cache average " + cacheAverage.getAsDouble() / 1000 + "μs");
-    System.out.println("NoGeneric average " + noGenericAverage.getAsDouble() / 1000 + "μs");
+    System.out.println("Cache average " + cacheAverage.getAsDouble() + "ms");
+    System.out.println("NoGeneric average " + noGenericAverage.getAsDouble() + "ms");
   }
 
   public static void runTest(Object o, int runs) {
-    long time = System.nanoTime();
+    long time = System.currentTimeMillis();
     for (int i = 0; i < runs; i++) {
       TiCache.me(o);
     }
-    time = System.nanoTime() - time;
+    time = System.currentTimeMillis() - time;
     System.out.println("Cache: " + time / 1000 + "μs");
     cacheTimes.add(time);
 
-    time = System.nanoTime();
+    time = System.currentTimeMillis();
     for (int i = 0; i < runs; i++) {
       TiNoGeneric.me((C1) o);
     }
-    time = System.nanoTime() - time;
+    time = System.currentTimeMillis() - time;
     System.out.println("NoGeneric: " + time / 1000 + "μs");
     noGenericTimes.add(time);
   }
